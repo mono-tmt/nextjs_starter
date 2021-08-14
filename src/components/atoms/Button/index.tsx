74c4903ce109cae53f0c08ad10018e13703b04a0
monoharada <harada@monosus.co.jp>
2021-08-14 20:01:20 +0900

    ボタンコンポーネントをちょっと変えてみた

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import styles from './Button.module.css'
 
 export interface ButtonProps {
   primary?: boolean;
   backgroundColor?: string;
   size?: "small" | "medium" | "large";
   label: string;
-  onClick?: () => void;
+  onClick?: (e?:any) => void;
 }
-
+// console.log(styles)
 export const Button: React.FC<ButtonProps> = ({
   primary = false,
   size = "medium",
@@ -15,29 +16,13 @@ export const Button: React.FC<ButtonProps> = ({
   label,
   ...props
 }) => {
+  const isPrimary = primary ? 'primary' : 'default'
   const baseButton = "rounded-full font-bold";
-  const sizeMode =
-    size === "small"
-      ? "py-1.5 px-4 text-xs"
-      : size === "medium"
-      ? "py-2 px-5 text-sm"
-      : size === "large"
-      ? "py-3 px-6 text-base"
-      : "";
-  return primary ? (
-    <div>
-      <button
-        type="button"
-        className={`text-white bg-blue-450 ${baseButton} ${sizeMode}`}
-        {...props}
-      >
-        {label}
-      </button>
-    </div>
-  ) : (
+  return (
+  
     <button
       type="button"
-      className={`text-gray-600 bg-transparent shadow-inner ${baseButton} ${sizeMode}`}
+      className={`${styles[isPrimary]} ${baseButton} ${styles[size]}`}
       style={{ backgroundColor }}
       {...props}
     >
