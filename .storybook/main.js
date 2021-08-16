06aea5cf31c5a225939f6d5b04a721f3cf890828
monoharada <harada@monosus.co.jp>
2021-08-16 10:17:34 +0900

    storybook のaddon追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,13 @@
+
 module.exports = {
   stories: ["../src/components/**/**/*.stories.@(ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
+    '@whitespace/storybook-addon-html',
+    '@storybook/addon-viewport',
+    'storybook-css-modules-preset',
     {
       name: "@storybook/addon-postcss",
       options: {
